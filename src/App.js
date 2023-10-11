7ef357bbdf00e0d627b50cf9a99eded38ac3e5c6
Kamaraj Kannan <kamarajKannan23@gmail.com>
2023-10-12 01:02:48 +0530

    image axios call commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import React, {useState, useEffect} from 'react';
 import AppBar from "./components/AppBar";
 import Loader from "./components/Loader";
 import UnsplashImages from "./components/UnsplashImages";
+import axios from 'axios';
+import { apiKey } from './apiKey';
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const apiRoot ="https://api.unsplash.com";
+    const accessKey = process.env.REACT_APP_ACCESSKEY;
+    
+    axios.get(`${apiRoot}/photos/random?client_id=${accessKey}&count=10`)
+    .then(res => setImages([...images, ...res.data]))
+  },[])
+
+
   return (
     <div className="App">
       <AppBar></AppBar>
       <Loader></Loader>
-      <UnsplashImages></UnsplashImages>
+      {images.map(images => (
+        <UnsplashImages url={images.urls.thumb} key={images.id}/>
+      ))}
     </div>
   );
 }
